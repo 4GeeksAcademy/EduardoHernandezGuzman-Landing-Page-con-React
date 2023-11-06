0e5d829a77212279564fa85e72f67501e90065b9
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-11-06 12:36:33 +0000

    Creación del componente card.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./nav.jsx"
 import Jumbrotron from "./jumbrotron.jsx";
+import Card from "./card.jsx";
 
 
 //create your first component
@@ -9,6 +10,23 @@ const Home = () => {
 		<>
 			<Nav></Nav>
 			<Jumbrotron></Jumbrotron>
+			
+			
+				<div class="row">
+					<div class="col-3">
+					<Card></Card>
+					</div>
+					<div class="col-3">
+					<Card></Card>
+					</div>
+					<div class="col-3">
+					<Card></Card>
+					</div>
+					<div class="col-3">
+					<Card></Card>
+					</div>
+				</div>
+			
 		</>
 	);
 };
--- src/js/component/jumbrotron.jsx
+++ src/js/component/jumbrotron.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Jumbrotron = () => {
     return (
         <>
-            <div className="position-relative p-5 text-muted  border-dashed rounded-5 mt-3 mx-3 "style={{ backgroundColor: "#ebebeb" }}>
+            <div className="position-relative p-5 text-muted  border-dashed rounded-5 mt-3 mx-4 "style={{ backgroundColor: "#ebebeb" }}>
                 <button type="button" className="position-absolute top-0 end-0 p-3 btn-close bg-secondary bg-opacity-10 rounded-pill" aria-label="Close"></button>
                 <svg className="bi mt-5 mb-3" width="48" height="48"><use xlinkHref="#check2-circle"></use></svg>
                 <h1  style={{ fontSize: "4rem", color: "#000"}}>A Warm Wellcome!</h1>
